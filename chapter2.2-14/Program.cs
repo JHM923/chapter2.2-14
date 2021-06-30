b07aa4b5115153ed7197717a77748f404b3b1b24
username <1151555861@qq.com>
2021-06-30 17:28:28 +0800

    first commit

---
+++ chapter2.2-14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace chapter2._2_14
+{
+    class Program
+    {
+        static IEnumberable<string> Iterator()
+        {
+            try{
+                Console.WriteLine("Before first yield");
+                yield return "first";
+                Console.WriteLine("Between yields");
+                yield return "second";
+                Console.WriteLine("After second yield");
+            }
+            finally
+            {
+                Console.WriteLine("In finally block");
+            }
+        }
+        static void Main()
+        {
+           foreach (string value in Iterator())
+           {
+               Console.WriteLine("Received value: {0}", value);
+           }
+           Console.ReadKey();
+        }
+    }
+}
